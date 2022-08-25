7ec829cde92b7a7e48485d589fe7469259c8f0a0
augude <augustgude@gmail.com>
2022-08-25 15:06:55 +0200

    Added lables to the plot

--- plotting.py
+++ plotting.py
@@ -9,10 +9,11 @@ x = data[:, 0]
 y = data[:, 1]
 
 fig, axs = plt.subplots(nrows = 1, ncols = 1, figsize = (8, 8))
-axs.plot(x, y, linestyle = "--", color = "blue")
-axs.scatter(x, y, s = 10, color = "red")
+axs.scatter(x, y, s = 10, color = "red", label = "Discrete values")
+axs.plot(x, y, linestyle = "-", color = "blue", label = "Fitted line")
 axs.set_xlabel("x")
 axs.set_ylabel(r"$e^x$")
 fig.tight_layout()
+plt.legend()
 plt.savefig("plott1.pdf")
 
