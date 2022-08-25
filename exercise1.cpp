09792a1af31ff9246996bbf10dc9e91f6136d9eb
augude <augustgude@gmail.com>
2022-08-25 13:13:37 +0200

    Added my first files with C++ code

---
+++ exercise1.cpp
@@ -0,0 +1,7 @@
+#include "exercise1.hpp"
+
+void myFunc(int number){
+    int newNumber = number + 10;
+    std::cout << "The input number was: " << number << "\n"; 
+    std::cout << "The new number is: " << newNumber << "\n";
+}
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "exercise1.hpp"
+
+int main(){
+    int n = 10;
+    myFunc(n);
+}
