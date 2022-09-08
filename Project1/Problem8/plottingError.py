0faad600344585bb0f88e3324ce7b6bf91408b23
augude <augustgude@gmail.com>
2022-09-08 10:37:57 +0200

    Added a beautiful loglog plot to problem 8

--- Project1/Problem8/plottingError.py
+++ Project1/Problem8/plottingError.py
@@ -34,15 +34,17 @@ lgd = axs.legend(loc = 'upper center', ncol = len(files)//2, fancybox = True
 plt.show()
 fig.savefig('errors.pdf', bbox_extra_artists=(lgd,), bbox_inches='tight')
 
-relErrorMax = np.zeros_like(files)
+relErrorMax = np.zeros(len(files))
+steps = np.zeros(len(files))
 fig, axs = plt.subplots(nrows = 1, ncols = 1, figsize = (10, 10))
 for index, filename in enumerate(files):
     data = np.loadtxt(filename) #loads the approximations from txt file
     x = data[0, :][1:-1] #extracts the x-points and discards the boundary conditions
     v = data[1, :][1:-1] #the approximation
     u = exact(x) #calculates the exact solution 
-    relerror = ( np.abs( (u - v)/u ) ) #reletive error 
+    relerror = ( np.abs( (u - v)/u ) ) #relative error 
     relErrorMax[index] = np.max(relerror)
+    steps[index] = (10**(index + 1))
     axs.semilogy(x, relerror, label = f'$n = {10**(index + 1)}$')
 axs.set_xlabel(r'$x$')
 axs.set_ylabel(r'$\log_{10}(\epsilon)$')
@@ -51,17 +53,17 @@ lgd = axs.legend(loc = 'upper center', ncol = len(files)//2, fancybox = True,
 fig.savefig('relerrors.pdf', bbox_extra_artists=(lgd,), bbox_inches='tight')
 plt.show()
 
-
+#%%
+print(steps)
 print(relErrorMax)
 
-"""fig, axs = plt.subplots(nrows = 1, ncols = 2, figsize = (12, 10), sharey = True)
-steps = 10**(np.arange(1, 8))
-axs[0].scatter(steps, relErrorMax)
-axs[1].semilogx(steps, relErrorMax)
-axs[0].set_xlabel(r'$n_{steps}$')
-axs[1].set_xlabel(r'$\log n_{steps}$')
-axs[0].set_ylabel(r'$\epsilon$')
-axs[0].set_yticks(np.linspace(10**(-6), 10**(-2), 10 ))
+fig, axs = plt.subplots(nrows = 1, ncols = 1, figsize = (12, 10))
+axs.scatter(steps, relErrorMax, s = 40)
+axs.plot(steps, relErrorMax, linestyle = "--", alpha = 0.5)
+axs.set_yscale('log')
+axs.set_xscale('log')
+axs.set_xlabel(r'$n_{steps}$')
+axs.set_xlabel(r'$\log_{10} n_{steps}$')
+axs.set_ylabel(r'$\log_{10} \epsilon$')
+fig.savefig('relerrorsmax.pdf', bbox_inches='tight')
 plt.show()
-"""# %%
-
