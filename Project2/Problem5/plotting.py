1d2567290c4cfb671f1a21b9dc08b01b9c76fca4
augude <augustgude@gmail.com>
2022-09-19 22:14:34 +0200

    Fitted the scaling data to a polynomial

--- Project2/Problem5/plotting.py
+++ Project2/Problem5/plotting.py
@@ -3,22 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import tikzplotlib
 import seaborn as sns
+from scipy.optimize import curve_fit
 
 sns.set_theme()
 
-data = np.loadtxt('countSimilaritiesDense.txt')
-
-listOfN = data[0, :]
-listOfIter = data[1, :]
-
-
-fig, axs = plt.subplots(ncols = 1, nrows = 1, figsize = (10, 10))
-axs.plot(listOfN, listOfIter)
-axs.set_xlabel(r'Size of matrix')
-axs.set_ylabel(r'Number of iterations')
-tikzplotlib.clean_figure()
-tikzplotlib.save(
-            f"convergenceDense.tex",
+files = np.array(['countSimilarities.txt', 'countSimilaritiesDense.txt'])
+for file in files:
+    data = np.loadtxt(file)
+    listOfN = data[0, :]
+    listOfIter = data[1, :]
+    
+    fig, axs = plt.subplots(ncols = 1, nrows = 1, figsize = (10, 10))
+    axs.plot(listOfN, listOfIter)
+    axs.set_xlabel(r'N')
+    axs.set_ylabel(r'I(N)')
+    tikzplotlib.clean_figure()
+    tikzplotlib.save(
+            f"{file[:-4]}.tex",
             extra_axis_parameters=[
                 "title style={align=center}",
                 "xmajorticks=true",
@@ -27,6 +28,39 @@ tikzplotlib.save(
             ],
             strict=True,
         )
-plt.show()
+    plt.show()
+
+# %%
+def fit(x, a, b, c, d):
+    return a*x**b + c*x**(b-1) + d
+
+files = np.array(['countSimilarities.txt'])
+for file in files:
+    data = np.loadtxt(file)
+    listOfN = data[0, :]
+    listOfIter = data[1, :]
+    
+    params, pcov = curve_fit(fit, listOfN, listOfIter)
+    print(params)
+    fitted = fit(listOfN, params[0], params[1], params[2], params[3])
+
+    fig, axs = plt.subplots(ncols = 1, nrows = 1, figsize = (10, 10))
+    axs.plot(listOfN, listOfIter, label = r'$I_(N)$')
+    axs.plot(listOfN, fitted, label = f'O(N^{params[1]:.2f})',linestyle = '--')
+    axs.set_xlabel(r'N') 
+    axs.set_ylabel(r'I(N)')
+    plt.legend()
+    tikzplotlib.clean_figure()
+    tikzplotlib.save(
+            f"fitted.tex",
+            extra_axis_parameters=[
+                "title style={align=center}",
+                "xmajorticks=true",
+                "ymajorticks=true",
+                "mark options={mark size=2.5pt, line width=1.5pt}",
+            ],
+            strict=True,
+        )
+    plt.show()
 
 # %%
