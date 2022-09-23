a9a72c9ea2d354e25edc250e5c1b34060e5b9de1
augude <augustgude@gmail.com>
2022-09-23 10:48:31 +0200

    Combined two plots into one

--- Project2/Problem5/plotting.py
+++ Project2/Problem5/plotting.py
@@ -7,35 +7,14 @@ from scipy.optimize import curve_fit
 
 sns.set_theme()
 
-files = np.array(['countSimilarities.txt', 'countSimilaritiesDense.txt'])
-for file in files:
-    data = np.loadtxt(file)
-    listOfN = data[0, :]
-    listOfIter = data[1, :]
-    
-    fig, axs = plt.subplots(ncols = 1, nrows = 1, figsize = (10, 10))
-    axs.plot(listOfN, listOfIter)
-    axs.set_xlabel(r'N')
-    axs.set_ylabel(r'I(N)')
-    tikzplotlib.clean_figure()
-    tikzplotlib.save(
-            f"{file[:-4]}.tex",
-            extra_axis_parameters=[
-                "title style={align=center}",
-                "xmajorticks=true",
-                "ymajorticks=true",
-                "mark options={mark size=2.5pt, line width=1.5pt}",
-            ],
-            strict=True,
-        )
-    plt.show()
-
-# %%
 def fit(x, a, b, c, d):
     return a*x**b + c*x**(b-1) + d
 
-files = np.array(['countSimilarities.txt'])
-for file in files:
+
+files = np.array(['countSimilarities.txt', 'countSimilaritiesDense.txt'])
+fig, axs = plt.subplots(ncols = 2, nrows = 1, figsize = (10, 10), sharey = True)
+
+for index, file in enumerate(files):
     data = np.loadtxt(file)
     listOfN = data[0, :]
     listOfIter = data[1, :]
@@ -43,24 +22,25 @@ for file in files:
     params, pcov = curve_fit(fit, listOfN, listOfIter)
     print(params)
     fitted = fit(listOfN, params[0], params[1], params[2], params[3])
-
-    fig, axs = plt.subplots(ncols = 1, nrows = 1, figsize = (10, 10))
-    axs.plot(listOfN, listOfIter, label = r'$I_(N)$')
-    axs.plot(listOfN, fitted, label = f'O(N^{params[1]:.2f})',linestyle = '--')
-    axs.set_xlabel(r'N') 
-    axs.set_ylabel(r'I(N)')
-    plt.legend()
-    tikzplotlib.clean_figure()
-    tikzplotlib.save(
-            f"fitted.tex",
-            extra_axis_parameters=[
-                "title style={align=center}",
-                "xmajorticks=true",
-                "ymajorticks=true",
-                "mark options={mark size=2.5pt, line width=1.5pt}",
-            ],
-            strict=True,
-        )
-    plt.show()
+    
+    axs[index].plot(listOfN, listOfIter)
+    axs[index].plot(listOfN, fitted, linestyle = '--')
+    axs[index].set_xlabel(r'N')
+    axs[index].legend(['I(N)', r'$I_{fit} = \mathcal{O}(N^2)$'])
+axs[0].set_ylabel(r'I(N)')
+axs[0].text(x = 0, y = 16500, s = 'Tridiagonal matrix')
+axs[1].text(x = 0, y = 16500, s = 'Dense matrix')
+tikzplotlib.clean_figure()
+tikzplotlib.save(
+    f"{file[:-4]}.tex",
+    extra_axis_parameters=[
+        "title style={align=center}",
+        "xmajorticks=true",
+        "ymajorticks=true",
+        "mark options={mark size=2.5pt, line width=1.5pt}",
+        ],
+        strict=True,
+    )
+plt.show()
 
 # %%
