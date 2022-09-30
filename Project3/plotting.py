2e7b21dd317e8393e4c993b8b54f025b8b3f5f4d
augude <augustgude@gmail.com>
2022-09-30 14:00:59 +0200

    Some small changes

--- Project3/plotting.py
+++ Project3/plotting.py
@@ -32,13 +32,14 @@ for i in range(N**2):
     Ez[ind1, ind2] = data[i, 7]  
     V[ind1, ind2] = data[i, 8]       
     
-fig, axs = plt.subplots(ncols = 1, nrows = 1, figsize = (10, 10))
+fig, axs = plt.subplots(ncols = 1, nrows = 1, figsize = (12, 10))
 plt.contourf(Y, Z, V, cmap = 'cool')
 cbar = plt.colorbar(cmap = 'cool')
 cbar.set_label(r'Electric potential [$\frac{u(\mu m)^2}{(\mu s)^2 e}$]')
 axs.quiver(Y[::5, ::5], Z[::5, ::5], Ey[::5, ::5], Ez[::5, ::5])
 axs.set_xlabel(r'y [$\mu m$]')
 axs.set_ylabel(r'z [$\mu m$]')
+fig.tight_layout()
 plt.savefig('PenningTrapSetup.pdf')
 plt.show()
 
