be60395d9393f08dab3e5c395a80ae355be570b6
Jakob <Jakob Sonstebo>
2022-11-07 17:52:10 +0100

    Pyarma saves and loads

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -1,31 +1,33 @@
+#include <time.h>
+
+#include <vector>
+
 #include "include/lattice.hpp"
 #include "include/mcmc.hpp"
 #include "include/utils.hpp"
-#include <vector>
-#include <time.h>
 #include "omp.h"  //OpenMP header
 using namespace std;
-using namespace arma; 
+using namespace arma;
 using namespace chrono;
 
+int main(int argc, char* argv[]) {
+    srand(time(NULL));  // set random seed
+    string testString = argv[1];
 
-int main(int argc, char* argv[]){
-    srand(time(NULL)); //set random seed
-    string testString  = argv[1];
-
-    if (testString == "validation2x2"){
-        //N samples from a 2x2 grid in order to compare with analytical results 
-        int N = 1e6; //number of MC cycles
-        const int L = 2; //size of lattice
+    if (testString == "validation2x2") {
+        // N samples from a 2x2 grid in order to compare with analytical results
+        int N = 1e6;      // number of MC cycles
+        const int L = 2;  // size of lattice
         double T = 1.0;
         string filename = "output/validation2x2.bin";
         mcmc(T, L, N, false, filename);
     }
 
-    else if (testString == "burnIn"){
-        //N samples from a 20x20 grid at different initial conditions and at different temperatures to investigate burn-in time
-        int N = 1e6; //number of MC cycles
-        const int L = 20; //size of lattice
+    else if (testString == "burnIn") {
+        // N samples from a 20x20 grid at different initial conditions and at
+        // different temperatures to investigate burn-in time
+        int N = 1e6;       // number of MC cycles
+        const int L = 20;  // size of lattice
         double T1 = 1.0;
         double T2 = 2.4;
         string filename1 = "output/ordered1.bin";
@@ -33,69 +35,73 @@ int main(int argc, char* argv[]){
         string filename3 = "output/unordered1.bin";
         string filename4 = "output/unordered2.bin";
 
-        vector<double> temp = {T1, T2, T1, T2}; 
+        vector<double> temp = {T1, T2, T1, T2};
         vector<string> filenames = {filename1, filename2, filename3, filename4};
         vector<bool> order = {true, true, false, false};
 
-        #pragma omp parallel
+#pragma omp parallel
         {
-            #pragma omp for
-            for (int i = 0; i < 4; i++){
-                mcmc(temp[i], L, N, order[i], filenames[i]); 
-            } //end parallelized loop
-        } //end parallelized region
+#pragma omp for
+            for (int i = 0; i < 4; i++) {
+                mcmc(temp[i], L, N, order[i], filenames[i]);
+            }  // end parallelized loop
+        }      // end parallelized region
     }
 
-    else if (testString == "histograms"){
-        //N samples from a 20x20 grid at 5 different temperatures to compare distributions of epsilon
-        int N = 1e6; //number of MC cycles
-        const int L = 20; //size of lattice
+    else if (testString == "histograms") {
+        // N samples from a 20x20 grid at 5 different temperatures to compare
+        // distributions of epsilon
+        int N = 1e6;       // number of MC cycles
+        const int L = 20;  // size of lattice
         vec temp = arma::linspace<vec>(1.0, 2.4, 5);
         string filename;
 
-        #pragma omp parallel
+#pragma omp parallel
         {
-            #pragma omp for
-            for (double t : temp){
-                filename = "output/L=20_" + to_string(omp_get_thread_num()) + ".bin";
-                mcmc(t, L, N, false, filename); 
-            } //end parallelized loop
-        } //end parallelized region
+#pragma omp for
+            for (int i = 0; i < 5; i++) {
+                filename =
+                    "output/L=" + to_string(L) + "_" + to_string(i) + ".bin";
+                mcmc(temp[i], L, N, false, filename);
+            }  // end parallelized loop
+        }      // end parallelized region
     }
 
-    else if (testString == "par"){
-        // 1 runs with a total of 1e7 samples from 2x2 grid with different number of threads (1 - 4) to find speed-up factor 
-        // the total number of samples is distributed evenly across the different threads
-        double T = 1.0; //temperature
+    else if (testString == "par") {
+        // 1 runs with a total of 1e7 samples from 2x2 grid with different
+        // number of threads (1 - 4) to find speed-up factor the total number of
+        // samples is distributed evenly across the different threads
+        double T = 1.0;  // temperature
         int L = 20;
         string filename = "output/test.bin";
         int numberThreads;
         auto t1 = high_resolution_clock::now();
-        #pragma omp parallel
+#pragma omp parallel
         {
             numberThreads = omp_get_num_threads();
-            int N = (double) 1e6/numberThreads;  //number steps per cycle
+            int N = (double)1e6 / numberThreads;  // number steps per cycle
             mcmc(T, L, N, false, filename);
-        } //end parallelized region  
+        }  // end parallelized region
         auto t2 = high_resolution_clock::now();
         auto duration = duration_cast<microseconds>(t2 - t1);
-        cout << scientificFormat(numberThreads) << scientificFormat(duration.count()) << endl; 
-        
-    }
-    else if (testString == "problem8"){
-        int N = 1e6; //number of MC cycles
-        const int L = atoi(argv[2]); //size of lattice
+        cout << scientificFormat(numberThreads)
+             << scientificFormat(duration.count()) << endl;
+
+    } else if (testString == "problem8") {
+        int N = 1e6;                  // number of MC cycles
+        const int L = atoi(argv[2]);  // size of lattice
         vec temp = arma::linspace<vec>(2.1, 2.4, 10);
         string filename;
-        #pragma omp parallel
+#pragma omp parallel
         {
-            #pragma omp for
-            for (int i = 0; i < 10; i++){
-                filename = "output/L=" + to_string(L) + "_" + to_string(i) + ".bin";
-                mcmc(temp[i], L, N, false, filename); 
-            } //end parallelized loop
-        } //end parallelized region
+#pragma omp for
+            for (int i = 0; i < 10; i++) {
+                filename =
+                    "output/L=" + to_string(L) + "_" + to_string(i) + ".bin";
+                mcmc(temp[i], L, N, false, filename);
+            }  // end parallelized loop
+        }      // end parallelized region
     }
-        
+
     return 0;
 }
--- Project4/src/mcmc.cpp
+++ Project4/src/mcmc.cpp
@@ -1,68 +1,72 @@
 #include "../include/mcmc.hpp"
 using namespace std;
-using namespace arma; 
+using namespace arma;
 
-
-void mcCycle(Lattice& s, vector<int> randomi, vector<int> randomj, vector<double> r){
+void mcCycle(Lattice& s, vector<int> randomi, vector<int> randomj,
+             vector<double> r) {
     int L = s.L;
-    for (int i = 0; i < L*L; i++){
-        
-        int ed = -2*s.energyij(randomi[i], randomj[i]); //energy difference between the two states
-        double A = s.energyDiff[ed]; //acceptance probability from the energy difference using prerecorded values
-        
-        if (r[i] <= A){
-            //update the state, energy and magnetization
-            int md = -2*s.spins(randomi[i], randomj[i]); //magnatization difference between the two states
-            s.spins(randomi[i], randomj[i]) *= -1; //update the spin ensemble
-            s.E += ed; //update the energy 
-            s.M += md; //update the magnetization
+    for (int i = 0; i < L * L; i++) {
+        int ed =
+            -2 *
+            s.energyij(randomi[i],
+                       randomj[i]);  // energy difference between the two states
+        double A = s.energyDiff[ed];  // acceptance probability from the energy
+                                      // difference using prerecorded values
+
+        if (r[i] <= A) {
+            // update the state, energy and magnetization
+            int md = -2 * s.spins(randomi[i],
+                                  randomj[i]);      // magnatization difference
+                                                    // between the two states
+            s.spins(randomi[i], randomj[i]) *= -1;  // update the spin ensemble
+            s.E += ed;                              // update the energy
+            s.M += md;                              // update the magnetization
         }
     }
 }
 
-void mcmc(double T, int L, int numberCycles, bool ordered, string filename){
-   
+void mcmc(double T, int L, int numberCycles, bool ordered, string filename) {
     mt19937 generator;
-    //generator of random doubles on [0.0, 1.0)
-    uniform_real_distribution<double> uniformDouble = uniform_real_distribution<double>(0.0, 1.0);
-    //generator of random ints on [0, L - 1]
+    // generator of random doubles on [0.0, 1.0)
+    uniform_real_distribution<double> uniformDouble =
+        uniform_real_distribution<double>(0.0, 1.0);
+    // generator of random ints on [0, L - 1]
     uniform_int_distribution<> uniformInt(0, L - 1);
 
     unsigned int seed = chrono::system_clock::now().time_since_epoch().count();
     generator.seed(seed);
 
-    vector<int> randomi(L*L); 
-    vector<int> randomj(L*L);
-    vector<double> r(L*L);
+    vector<int> randomi(L * L);
+    vector<int> randomj(L * L);
+    vector<double> r(L * L);
 
+    // functions for filling vectors with random numbers
+    auto genInt = [&uniformInt, &generator]() { return uniformInt(generator); };
 
-    //functions for filling vectors with random numbers
-    auto genInt = [&uniformInt, &generator](){
-                return uniformInt(generator);
-            };
+    auto genDouble = [&uniformDouble, &generator]() {
+        return uniformDouble(generator);
+    };
 
-    auto genDouble = [&uniformDouble, &generator](){
-                return uniformDouble(generator);
-            };
-
-    //initialize a lattice of size LxL at temperature T 
+    // initialize a lattice of size LxL at temperature T
     Lattice s = Lattice(L, T, ordered);
 
-    mat EM = mat(numberCycles, 2);
+    mat EM = mat(numberCycles + 1, 2);
+    EM(0, 0) = L;
+    EM(0, 1) = T;
 
-    s.energyMagnetization(); //initialize E and M
-    
-    for (int i = 0; i < numberCycles; i++){
+    s.energyMagnetization();  // initialize E and M
+
+    for (int i = 0; i < numberCycles; i++) {
         generate(begin(randomi), end(randomi), genInt);
         generate(begin(randomj), end(randomj), genInt);
         generate(begin(r), end(r), genDouble);
 
-        mcCycle(s, randomi, randomj, r); //do one whole mc cycle and update the state, energy and magnetization 
-        //write values to file
-        EM[i, 0] = s.E;
-        EM[i, 1] = s.M;
+        mcCycle(s, randomi, randomj, r);  // do one whole mc cycle and update
+                                          // the state, energy and magnetization
+        // write values to file
+        EM(i + 1, 0) = s.E;
+        EM(i + 1, 1) = s.M;
     }
-    EM.save(filename); //saving as binaryfile    
+    EM.save(filename);  // saving as binaryfile
 }
-    
-    
+
