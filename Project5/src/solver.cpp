e17966e8612c18c500e30ef2b64ff1ed3a9e71b8
JunmiHu <junmiao.hu1@gmail.com>
2022-11-15 10:00:17 +0100

    Fixed nomenclature to match project description

--- Project5/src/solver.cpp
+++ Project5/src/solver.cpp
@@ -6,7 +6,7 @@
 class Solver 
 {
     public:
-        int m, n; // Iter points for x,y axes and time
+        int M, N; // Iter points for x,y axes and time
         double dt;
         double h;
 
@@ -19,10 +19,10 @@ class Solver
         std::complex<double> r;
 
         /* Constructor */
-        Solver(int M, int N, double DT)
+        Solver(int M_in, int N_in, double DT)
         {
-            m = M;
-            n = N;
+            M = M_in;
+            N = N_in;
             dt = DT;
             h = 1./(N-1);
             r = std::complex<double>(0, dt/(2*h*h));
@@ -39,30 +39,30 @@ class Solver
             } */
 
             // Only internal points
-            u.zeros(m-2, m-2);
-            v.ones(m-2, m-2); //Initalise input matrix V with some constant
+            u.zeros(M-2, M-2);
+            v.ones(M-2, M-2); //Initalise input matrix V with some constant
 
         }
 
         /* Converting double index to single index */
         int pair_to_single(int i, int j)
         {
-            return j*(m-2) + i;
+            return j*(M-2) + i;
         }
 
         /* Filling matrices for Crank-Nicholson */
         void fill_matrices()
         {
-            A.zeros((m-2)*(m-2), (m-2)*(m-2));
-            B.zeros((m-2)*(m-2), (m-2)*(m-2));
-            arma::cx_vec a((m-2)*(m-2));
-            arma::cx_vec b((m-2)*(m-2));
+            A.zeros((M-2)*(M-2), (M-2)*(M-2));
+            B.zeros((M-2)*(M-2), (M-2)*(M-2));
+            arma::cx_vec a((M-2)*(M-2));
+            arma::cx_vec b((M-2)*(M-2));
 
             std::complex<double> c_b;
             int k;
 
-            for (int i = 0; i < m-2; i++){
-                for (int j = 0; j < m-2; j++){
+            for (int i = 0; i < M-2; i++){
+                for (int j = 0; j < M-2; j++){
                     k = pair_to_single(i, j);
                     std::complex<double> c_a(1.0, 4.0*r.imag() + 0.5*dt*v.at(i,j));
                     c_b = std::conj(c_a);
@@ -79,16 +79,16 @@ class Solver
             B.diag() = b;
 
 
-            for (int i = (m-2); i < (m-2)*(m-2); i++){
-                A.at(i - (m-2), i) = -r;
-                A.at(i, i - (m-2)) = -r;
+            for (int i = (M-2); i < (M-2)*(M-2); i++){
+                A.at(i - (M-2), i) = -r;
+                A.at(i, i - (M-2)) = -r;
 
-                B.at(i - (m-2), i) = r;
-                B.at(i, i - (m-2)) = r;
+                B.at(i - (M-2), i) = r;
+                B.at(i, i - (M-2)) = r;
             }
 
-            for (int i = 1; i < (m-2)*(m-2); i++){
-                if (i % (m-2) != 0){
+            for (int i = 1; i < (M-2)*(M-2); i++){
+                if (i % (M-2) != 0){
                     A.at(i-1, i) = -r;
                     A.at(i, i-1) = -r;
 
